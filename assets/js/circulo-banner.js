43832c39caeabe9df7b331c400bf2c3ef2ed916d
lucanmu <lucanmu@hotmail.com>
2024-07-28 18:59:44 -0300

    teste circulo

--- assets/js/circulo-banner.js
+++ assets/js/circulo-banner.js
@@ -7,17 +7,21 @@ document.addEventListener("DOMContentLoaded", function() {
     svg.setAttribute("xmlns", svgNS);
     svg.setAttribute("viewBox", "0 0 150 150");
     svg.setAttribute("version", "1.1");
+    svg.setAttribute("width", "100%");
+    svg.setAttribute("height", "100%");
 
     // Cria o caminho circular
     const path = document.createElementNS(svgNS, "path");
     path.setAttribute("id", "textPath");
-    path.setAttribute("d", "M 0,75 a 75,75 0 1,1 0,1 z");
+    path.setAttribute("d", "M 75,75 m -75,0 a 75,75 0 1,1 150,0 a 75,75 0 1,1 -150,0");
 
     // Cria o elemento de texto
     const text = document.createElementNS(svgNS, "text");
     const textPath = document.createElementNS(svgNS, "textPath");
     textPath.setAttribute("href", "#textPath");
-    textPath.textContent = "teste";
+    textPath.setAttribute("startOffset", "50%");
+    textPath.setAttribute("text-anchor", "middle");
+    textPath.textContent = "UX Design Brand e Website";
 
     // Anexa os elementos juntos
     text.appendChild(textPath);
